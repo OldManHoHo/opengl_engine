ca9a90f22bf82064a9d37092d0f277ab71b381a1
tjcw <teddyjwalsh@gmail.com>
2018-12-05 07:26:14 +0000

    Improved TGLUDPInterface

--- opengl_engine/TGLUDPInterface.cpp
+++ opengl_engine/TGLUDPInterface.cpp
@@ -1,4 +1,6 @@
 #include "TGLUDPInterface.h"
+#include <unistd.h>
+#include <iostream>
 
 TGLUDPInterface::TGLUDPInterface()
 {
@@ -7,12 +9,13 @@ TGLUDPInterface::TGLUDPInterface()
 
 bool TGLUDPInterface::init(std::string ip_address, int port)
 {
-	my_address = new sockaddr_in;
-	my_address->sin_family = AF_INET;
-	my_address->sin_port = htons(port);	
-	inet_aton(ip_address.c_str(), &my_address->sin_addr);
+	int retval = 0;
+	my_address.sin_family = AF_INET;
+	my_address.sin_port = htons(port);	
+	inet_aton(ip_address.c_str(), &my_address.sin_addr);
 	sock = socket(AF_INET, SOCK_DGRAM, 0);	
-	bind(sock, (struct sockaddr *)my_address, sizeof(my_address));
+	retval = bind(sock, (sockaddr *)&my_address, sizeof(my_address));
+	return retval;
 }
 
 int TGLUDPInterface::send_msg(std::vector<uint8_t> in_msg, sockaddr_in * to_addr)
@@ -25,9 +28,11 @@ int TGLUDPInterface::send_msg(std::vector<uint8_t> in_msg, sockaddr_in * to_addr
 
 std::vector<uint8_t> TGLUDPInterface::receive_msg(sockaddr_in * from_addr)
 {
+	std::cout << "Receiving..." << "\n";
 	std::vector <uint8_t> out_vec(max_msg_size);
 	socklen_t out_size;
 	recvfrom(sock, &out_vec[0], max_msg_size, 0, (struct sockaddr *)from_addr, &out_size);
+	std::cout << "COTGDSFS" << "\n";
 }
 
 void TGLUDPInterface::start_receive_thread()
@@ -41,25 +46,58 @@ void TGLUDPInterface::receive_loop()
 	while(1)
 	{
 		std::vector <uint8_t> rec_msg;
-		sockaddr_in * from_addr;
-		rec_msg = receive_msg(from_addr);
+		sockaddr_in from_addr;
+		rec_msg = receive_msg(&from_addr);
 		msg_queue_mutex.lock();
-		msg_queue.push_back(rec_msg);
+		if (msg_qs.find(from_addr) == msg_qs.end())
+		{
+			msg_qs[from_addr] = std::deque<std::vector <uint8_t> >(0);
+		}
+		msg_qs[from_addr].push_back(rec_msg);
 		msg_queue_mutex.unlock();
 	}
 }
 
-std::vector <uint8_t> TGLUDPInterface::pop_msg()
+std::vector <uint8_t> TGLUDPInterface::pop_msg(sockaddr_in * in_addr)
 {
 	std::vector <uint8_t> out_vector;
 	msg_queue_mutex.lock();
-	out_vector = msg_queue.front();
+	out_vector = msg_qs[*in_addr].front();
 	msg_queue_mutex.unlock();
 	return out_vector;
 }
 
 int main()
 {
-	TGLUDPInterface udp_int;
-	udp_int.init("localhost", 12345);
+	std::vector <uint8_t> test_msg = {1,2,3,4,5,6,7,8,9};
+	int retval = 0;
+	TGLUDPInterface udp_int, udp_int2;
+	retval = udp_int.init("127.17.0.6", 12345);
+	if (retval != 0)
+	{
+		std::cout << "ERROR1" << "\n";
+	}
+	retval = udp_int2.init("127.17.0.6", 12346);
+	if (retval != 0)
+	{
+		std::cout << "ERROR2" << "\n";
+		std::cout << errno << "\n";
+	}
+	udp_int.start_receive_thread();
+	while(1)
+	{
+		sockaddr_in to_addr;
+		inet_aton("127.17.0.6", &to_addr.sin_addr);
+		to_addr.sin_port = 12345;
+		to_addr.sin_family = AF_INET;
+		retval = udp_int2.send_msg(test_msg, &to_addr);
+		std::cout << "Sent: " << retval << "\n";
+		sleep(3);
+		to_addr.sin_port = 12346;
+		std::vector <uint8_t> got = udp_int.pop_msg(&to_addr);
+		for (auto g : got)
+		{
+			std::cout << g << "\n";
+		}
+	}
 }
--- opengl_engine/TGLUDPInterface.h
+++ opengl_engine/TGLUDPInterface.h
@@ -11,13 +11,21 @@
 #include <deque>
 #include <algorithm>
 #include <set>
+#include <map>
 
 
-struct rec_msg
+struct udp_address
 {
-	sockaddr_in * from_addr;
-	std::vector <uint8_t> msg;
+	sockaddr_in addr;
+	udp_address(sockaddr_in in_addr) {addr = in_addr;}
+	bool operator< (const udp_address& other) const
+	{
+		return addr.sin_port*pow(2,32) + addr.sin_addr.s_addr < other.addr.sin_port*pow(2,32) + other.addr.sin_addr.s_addr;
+	}
 };
+
+
+
 
 class TGLUDPInterface
 {
@@ -25,7 +33,7 @@ public:
 	static const int max_msg_size = 1024;
 
 	int sock;
-	struct sockaddr_in * my_address;
+	struct sockaddr_in my_address;
 	std::vector <sockaddr_in> peers;
 	TGLUDPInterface();
 	bool init(std::string ip_address, int port);
@@ -33,14 +41,15 @@ public:
 	std::thread * receive_thread;
 	std::mutex msg_queue_mutex;
 	std::deque <std::vector<uint8_t> > msg_queue;
-	std::set <sockaddr_in> clients;
-	//std::map <sockaddr_in, std::deque<std::vector <uint8_t> > > msq_queues;
-	std::map <int, int> test;
+	//std::set <struct sockaddr_in> clients;
+	//std::unordered_map <struct sockaddr_in, std::deque<std::vector <uint8_t> > > msg_qs;
+	std::map <udp_address,std::deque<std::vector <uint8_t> > > msg_qs;
+	//std::map <int, int> msg_qs;
 
 	int send_msg(std::vector<uint8_t> in_msg, sockaddr_in * to_addr);
 	int receive_msg(std::vector <uint8_t> out_msg, std::string& ip_address, int& port);
 	std::vector<uint8_t> receive_msg(sockaddr_in * from_addr);
 	void start_receive_thread();
 	void receive_loop();
-	std::vector <uint8_t> pop_msg();
+	std::vector <uint8_t> pop_msg(sockaddr_in * in_addr);
 };	
